d455a0721855e4567bfd716e4ae5a3249af2c787
Tuukka Bordi <m2296@student.jamk.fi>
2021-09-08 22:41:51 +0300

    Assignment 1 part 1 done

---
+++ ass1/lib/ass1.ex
@@ -0,0 +1,36 @@
+defmodule Ass1 do
+  @moduledoc """
+  Documentation for `Ass1`.
+  """
+
+  @doc """
+  Instruction, Part 1:
+
+  Write an Elixir script that declares a variable and sets its value to 123.
+  Print the value of the variable to the console.
+  Add code that asks a text line from the user (use IO.gets).
+  Add text <- You said that to the text that user entered.
+  Print the combined text into the console.
+
+  ## Examples
+
+      iex(1)> c "lib/ass1.ex"
+      [Ass1]
+      iex(2)> Ass1.part1()
+      Value of the variable is 123
+      Type some text
+      This is text
+      You just typed This is text
+
+      :ok
+      iex(3)>
+
+  """
+  def part1 do
+    someNumber = 123
+    IO.puts("Value of the variable is #{someNumber}")
+    someInput = IO.gets("Type some text\n")
+    IO.puts("You just typed #{someInput}")
+
+  end
+end
